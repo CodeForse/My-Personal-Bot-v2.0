e07f78ed9a44861667ae4cfd6d661207df74bd88
Aslan <45170182+CodeForse@users.noreply.github.com>
2022-11-29 18:54:25 +0600

    refactor BD connect alchemy

--- Bd_connect.py
+++ Bd_connect.py
@@ -1,23 +1,29 @@
-import psycopg2
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
 from constants import *
-import pydantic
 
-conn = psycopg2.connect(host="localhost",
-    database=KEYS.db_name,
-    user=KEYS.db_login,
-    password=KEYS.db_pass)
 
-class DataBase():
-    conn = None
-    cursor = None 
+Base = declarative_base()
 
-    def __init__(self) -> None:
-        self.conn = psycopg2.connect(host="localhost",
-        database=KEYS.db_name,
-        user=KEYS.db_login,
-        password=KEYS.db_pass)
-        
-        self.cursor = self.conn.cursor()
+class Instruction(Base):
+    __tablename__ = 'Instructions'
+    engine = create_engine(f'postgresql+psycopg2://{KEYS.db_login}:{KEYS.db_pass}@localhost:5432/{KEYS.db_name}',\
+        echo=True, future=True)
+
+    id = Column(Integer, primary_key = True)
+    user_id = Column(Integer)
+    key_text = Column(String)
+    message_id = Column(Integer)
+
+    def __repr__(self) -> str:
+        return f'Instruction(id={self.id!r}, user_id={self.user_id!r}, key_text={self.key_text!r}, message_id={self.message_id!r})'
     
+    def create_table(self):
+        Base.metadata.create_all(self.engine)
     
 
+    
+Instruction().create_table()
---
+++ prev/Bd_connect.py
@@ -0,0 +1,24 @@
+import psycopg2
+from constants import *
+import pydantic
+
+conn = psycopg2.connect(host="localhost",
+    database=KEYS.db_name,
+    user=KEYS.db_login,
+    password=KEYS.db_pass)
+
+class DataBase():
+    conn = None
+    cursor = None 
+
+    def __init__(self) -> None:
+        self.conn = psycopg2.connect(host="localhost",
+        database=KEYS.db_name,
+        user=KEYS.db_login,
+        password=KEYS.db_pass)
+        
+        self.cursor = self.conn.cursor()
+    
+    # def add_row(self, ): 
+
+
